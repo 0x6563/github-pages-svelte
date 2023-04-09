dc538441ad40315d6df5324ca70da55a84602905
0x6563 <63897158+0x6563@users.noreply.github.com>
2023-04-09 16:57:02 -0400

    init

---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+const { GITHUB_REPOSITORY } = process.env; // ie. octocat/hello-world
+const base = GITHUB_REPOSITORY ? GITHUB_REPOSITORY.substring(GITHUB_REPOSITORY.indexOf('/')) : '';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+	onwarn(warning, handler) {
+		if (!warning.code.indexOf('a11y')) return;
+		handler(warning);
+	},
+	kit: {
+		paths: { assets: "", base },
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: 'index.html'
+		})
+	}
+};
+
+export default config;
